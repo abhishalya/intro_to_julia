1258c9ea40ca95e9151d8ddab15cc4bcaf5a9f76
Abhinav Kaushlya <abhinavkaushlya@gmail.com>
2020-10-15 21:13:12 +0530

    Add few final changes to the notebook

--- notebook.jl
+++ notebook.jl
@@ -128,11 +128,14 @@ x = 1:10; y = rand(10)
 # ╔═╡ f5600526-0eba-11eb-243f-ffe32c8a4c1e
 plot(x, y)
 
+# ╔═╡ f558d724-0ef1-11eb-3777-3f417a72b58d
+md"# "
+
 # ╔═╡ fb6f9b84-0eba-11eb-07c0-0d85d8377eb5
 plot(Plots.fakedata(50, 5), w = 2)
 
 # ╔═╡ a9151da2-0ebb-11eb-238b-4de8054a23c9
-theme(:default)
+theme(:solarized)
 
 # ╔═╡ 335968c6-0ebc-11eb-310f-31a13429441d
 md"""# DifferentialEquations.jl
@@ -173,6 +176,9 @@ g_1 = path_graph(6)
 # ╔═╡ ed0f5cfc-0ec2-11eb-2e68-71834cf8fe1d
 gplot(g_1)
 
+# ╔═╡ 065fc5a8-0ef2-11eb-326d-a706ed46bff2
+md"# "
+
 # ╔═╡ 0758d3f0-0ec2-11eb-1cce-dd57ef093259
 g_2 = SimpleWeightedGraph(10)
 
@@ -200,11 +206,14 @@ md"""# Franklin.jl
 # ╔═╡ be3d1dae-0ec4-11eb-3712-1f370b47457b
 newsite("My new site", template="pure-sm")
 
-# ╔═╡ 9a93ee74-0ec5-11eb-1ebd-4708eb1480d1
-cd("My new site")
-
 # ╔═╡ 486c8028-0ec5-11eb-0ee3-d16eaf57ae77
 readdir()
+
+# ╔═╡ bcb6705c-0ef1-11eb-1ab2-93c4095bdd37
+md"""# Pluto.jl
+
+### This presentation is made with Pluto.jl!
+"""
 
 # ╔═╡ e66f3dc4-0ec5-11eb-18bd-05905909377a
 md"""# Hush, there are a lot of these
@@ -225,6 +234,9 @@ md"""# Julia, is it better than python?
 # ╔═╡ a3992086-0ec6-11eb-3e22-3745ab03e956
 load("./images/picture_1.png")
 
+# ╔═╡ 4926e5d8-0ef2-11eb-2550-bd608cb98950
+md"# "
+
 # ╔═╡ aab7782c-0ecb-11eb-2d06-8b1ee9d60b5a
 load("./images/picture_2.png")
 
@@ -233,6 +245,9 @@ md"""# """
 
 # ╔═╡ 220ab8bc-0ecc-11eb-3c41-33b1e564486a
 load("./images/picture_3.png")
+
+# ╔═╡ 53cc2dd8-0ef2-11eb-2b79-8572c61d90e9
+md"# "
 
 # ╔═╡ 24f89fee-0ecc-11eb-1525-cddda435e969
 load("./images/picture_4.png")
@@ -260,7 +275,7 @@ ps: please don't ask me tough ones :P
 """
 
 # ╔═╡ 26aa052c-0ecd-11eb-1df6-db2143e34471
-println("Hello world!")
+println("thank you!")
 
 # ╔═╡ Cell order:
 # ╟─378c94c0-0e32-11eb-2781-898c22ee1f9a
@@ -289,6 +304,7 @@ println("Hello world!")
 # ╠═761f51f0-0eb7-11eb-256e-c92b976c2337
 # ╠═7abc8296-0eb1-11eb-2143-299bb5273810
 # ╠═f5600526-0eba-11eb-243f-ffe32c8a4c1e
+# ╟─f558d724-0ef1-11eb-3777-3f417a72b58d
 # ╠═fb6f9b84-0eba-11eb-07c0-0d85d8377eb5
 # ╠═a9151da2-0ebb-11eb-238b-4de8054a23c9
 # ╟─335968c6-0ebc-11eb-310f-31a13429441d
@@ -304,6 +320,7 @@ println("Hello world!")
 # ╠═e54f8498-0ec1-11eb-1756-6f29b7d0897e
 # ╠═14f119cc-0ec3-11eb-3bca-994fb5f6b123
 # ╠═ed0f5cfc-0ec2-11eb-2e68-71834cf8fe1d
+# ╟─065fc5a8-0ef2-11eb-326d-a706ed46bff2
 # ╠═febda3f6-0ec1-11eb-02d1-05a4caaddc82
 # ╠═0758d3f0-0ec2-11eb-1cce-dd57ef093259
 # ╠═27cd3d5a-0ec3-11eb-0830-8f08536cd744
@@ -311,15 +328,17 @@ println("Hello world!")
 # ╟─a2f8da16-0ec3-11eb-2d75-2d996650a7a8
 # ╠═ab229316-0ec4-11eb-0b00-a756c44cd0f2
 # ╠═be3d1dae-0ec4-11eb-3712-1f370b47457b
-# ╠═9a93ee74-0ec5-11eb-1ebd-4708eb1480d1
 # ╠═486c8028-0ec5-11eb-0ee3-d16eaf57ae77
+# ╟─bcb6705c-0ef1-11eb-1ab2-93c4095bdd37
 # ╟─e66f3dc4-0ec5-11eb-18bd-05905909377a
 # ╟─20bbfee0-0ec6-11eb-2503-7d18df89f6d0
 # ╟─53a6a35a-0ec9-11eb-3aec-e152cac5c4e4
 # ╟─a3992086-0ec6-11eb-3e22-3745ab03e956
+# ╟─4926e5d8-0ef2-11eb-2550-bd608cb98950
 # ╟─aab7782c-0ecb-11eb-2d06-8b1ee9d60b5a
 # ╟─3554232c-0ecc-11eb-1929-47354e855959
 # ╟─220ab8bc-0ecc-11eb-3c41-33b1e564486a
+# ╟─53cc2dd8-0ef2-11eb-2b79-8572c61d90e9
 # ╟─24f89fee-0ecc-11eb-1525-cddda435e969
 # ╟─4225747a-0ecc-11eb-19fc-61c792a7b733
 # ╟─28926e96-0ecc-11eb-215c-61b582c64036
